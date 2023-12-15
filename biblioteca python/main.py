93cf772065f73fb64bcafd28cf2f159b5d364b2e
AdryanMaikel <adryanmaikel@gmail.com>
2023-12-15 11:05:27 -0300

    adicionando lower e upper

--- biblioteca python/main.py
+++ biblioteca python/main.py
@@ -1,15 +1,36 @@
-string = 'Hello, World!'
-string_splited = string.split(',')
+# string = 'Hello, World!'
+# string_splited = string.split(',')
 # print(string_splited)
 
 # print(len(string_splited))
 
-string = 'foo'
-string2 = 'adryan'
+# string = 'foo'
+# string2 = 'adryan'
 # print(f'"{string}".lstrip() = "{string.lstrip()}"')
 # print(f'"{string}".rstrip() = "{string.rstrip()}"')
 # print(f'"{string}".strip() = "{string.strip()}"')
 
-print(f'"{string.rjust(10)}"')
-print(f'"{string.ljust(10)}"')
-print(f'"{string.upper()}"')
+# print(f'"{string.rjust(10)}"')
+# print(f'"{string.ljust(10)}"')
+# print(f'"{string.upper()}"')
+
+
+
+# string = 'aleatório'
+
+# for word in string:
+#   print(word)
+  
+# string = []
+
+# for word in range(len(string)):
+#   print(string[word])
+
+# print(list(string))
+
+
+while True:
+  print('FOO'.lower() , 'baa'.upper())
+  if input('digite (s) para sair: ').lower() == 's':
+    break
+  
